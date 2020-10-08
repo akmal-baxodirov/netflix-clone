49a9f0f08decaa4c6b9d2f4b7c6010646c9708ce
Akmal Baxodirov <bahodirov0609@gmail.com>
2020-10-08 19:59:08 +0500

    init: Completed project

---
+++ src/Row.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
+import axios from "./axios";
+import "./Row.css";
+import movieTrailer from "movie-trailer";
+
+const baseurl = "https://image.tmdb.org/t/p/original/";
+
+export const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      const res = await request.data.results;
+      setMovies(res);
+      return res;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer( movie?.original_name ||  movie?.name || movie?.title || movie?.original_title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posts">
+        {movies.map((movie) => (
+          <img
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseurl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+};
